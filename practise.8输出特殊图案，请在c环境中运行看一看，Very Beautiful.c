4b719a764c30eef2b15828a9dd213fd8cbeb4037
robot ljlang <146798517+jialangli@users.noreply.github.com>
2023-11-16 14:11:57 +0800

    输出特殊图案，请在c环境中运行看一看

---
+++ practise.8输出特殊图案，请在c环境中运行看一看，Very Beautiful.c
@@ -0,0 +1,21 @@
+//题目：输出特殊图案，请在c环境中运行，看一看，Very Beautiful!
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main(void)
+{
+    int i,j;
+    SetConsoleOutputCP(437);
+    char a=176,b=219;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(i==j||(i+j+1)==5)
+                printf("%c",b);
+            else
+                printf("%c%c",a,a);
+        }
+        printf("\n");
+    }
+}
