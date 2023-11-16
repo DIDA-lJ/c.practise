fa51efa658c625170f8cb0c7c9c863ea639bd965
robot ljlang <146798517+jialangli@users.noreply.github.com>
2023-11-16 14:12:15 +0800

    输出9乘9口诀

---
+++ practise.9输出9乘9口诀.c
@@ -0,0 +1,16 @@
+//Êä³ö9*9¿Ú¾÷¡£
+#include<stdio.h>
+int main(){
+    int i,j,result;
+    for(i=1;i<=9;i++){
+        for(j=1;j<=i;j++){
+            result=i*j;
+            printf("  %d*%d=%d",i,j,result);
+        }
+    printf("\n");
+    }
+return 0;
+}
+
+
+
