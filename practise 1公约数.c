2c4a2fb4001088472024fbb221319f30ef7d4ece
robot ljlang <146798517+jialangli@users.noreply.github.com>
2023-11-16 14:09:25 +0800

    Add files via upload

---
+++ practise 1公约数.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int gcd(int a,int b){
+    return (b==0)?a:gcd(b,a%b); 
+} 
+int main(){
+    int a=0,b=0;
+    while(a==0&&b==0){
+    printf("请输入a，b的值：");
+    scanf("%d%d",&a,&b);
+    printf("这两个数的公约数=%d\n",gcd(a,b)) ;
+    }
+    return 0;
+}
