41814d8bdedc0dd76061434d41fe75988f22a455
robot ljlang <146798517+jialangli@users.noreply.github.com>
2023-11-16 14:12:28 +0800

    要求输出国际象棋棋盘

---
+++ practise.10要求输出国际象棋棋盘.c
@@ -0,0 +1,29 @@
+//要求输出国际象棋棋盘。
+
+//程序分析：国际象棋棋盘由64个黑白相间的格子组成，分为8行*8列。
+//用i控制行，j来控制列，根据i+j的和的变化来控制输出黑方格，还是白方格。
+
+ #include<stdio.h>
+#include<stdlib.h>
+int  main()
+{
+    int  i,j;
+    for(i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+        {
+            if((i+j)%2==0)
+            {
+                printf("%c%c",0xa8,0x80);
+            }
+            else
+            {
+                printf("  ");
+            }  
+        }
+        printf("\n");
+    }
+    system("pause");
+    return  0;
+ 
+}
