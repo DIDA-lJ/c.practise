1b181e72d5aa00e3fc8f77b28b78ccfba0eb9a9a
LJ <97254796+DIDA-lJ@users.noreply.github.com>
2024-01-06 15:03:56 +0800

    Update practise 1公约数.c

--- practise 1公约数.c
+++ practise 1公约数.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+// 閫掑綊
 int gcd(int a,int b){
     return (b==0)?a:gcd(b,a%b); 
 } 
 int main(){
     int a=0,b=0;
     while(a==0&&b==0){
-    printf("请输入a，b的值：");
-    scanf("%d%d",&a,&b);
-    printf("这两个数的公约数=%d\n",gcd(a,b)) ;
+        printf("璇疯緭鍏锛宐鐨勫�硷細");
+        scanf("%d%d",&a,&b);
+        printf("杩欎袱涓暟鐨勫叕绾︽暟=%d\n",gcd(a,b)) ;
     }
     return 0;
 }
